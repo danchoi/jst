b29ea07479297b1ebf4e644812926429899b29a3
DHC <danchoi@danielchoi.com>
2018-06-09 18:18:31 -0400

    add optparse

--- Main.hs
+++ Main.hs
@@ -2,21 +2,45 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 import Parser
+import Control.Monad (when)
 import Eval
-import Data.Aeson
-import Data.Attoparsec.Text
+import Data.Aeson (eitherDecode, Value)
+import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import qualified Data.ByteString.Lazy.Char8 as BL8
 import System.Environment
+import Options.Applicative
+import System.IO
+
+data Options = Options {
+      debug :: Bool
+    , template :: FilePath
+    , jsonInput :: FilePath
+    }
+
+options :: Parser Options
+options = Options
+  <$> switch (short 'v' <> help "Debug parsing")
+  <*> strArgument (metavar "TEMPLATE")
+  <*> strArgument (metavar "JSONFILE" <> help "Use - for stdin")
+
+opts :: ParserInfo Options
+opts = info (helper <*> options)
+          (fullDesc <> header "jst" <> progDesc "simple json text templating")
 
 main :: IO ()
 main = do
-  tmplFile:_ <- getArgs
-  tmpl <- T.readFile tmplFile
+  o <- execParser opts
+  tmpl <- T.readFile $ template o
   let bs = either error id $ parseBlocks tmpl
-  print bs
-  v :: Value <- (either error id . eitherDecode) <$> BL8.getContents 
-  print v
+  s <- case jsonInput o of
+          "-" -> BL8.getContents 
+          fp -> BL8.readFile fp
+  let v :: Value = either error id . eitherDecode $ s
+  when (debug o) $ do
+    print bs
+    print v
   T.putStr $ evalTemplate v bs
+
 
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # jstmpl
+
+
+items.tmpl
+
+```
+## {{.title}}
+{{for item in .items-}}
+{{if $last}}and {{end}}{{item.name}}{{if (! $last) && $index != 1}},{{end-}}
+ {{end-}}
+
+{{for item in .items-}}
+* {{$index}}. {{item.name}} {{.title}} {{if $last}}LAST{{end}} {{$length}}
+{{end-}}
+
+hello
+```
+
+items.json
+
+```
+{"items":[
+  {"name":"apple"},
+  {"name":"orange"}
+  ],
+"title":"foods"
+}
+```
+
+Command:
+
+  jst items.tmpl items.json
+
+output:
+
+```
+## foods
+apple and orange
+
+* 1. apple foods  2
+* 2. orange foods LAST 2
+
+hello
+```
+
+
